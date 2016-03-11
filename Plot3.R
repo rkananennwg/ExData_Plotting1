7167df9920dc0cc52abdeb83019aa8b4688c4731
rkananennwg <rkananen@newgistics.com>
2016-03-10 21:29:05 -0600

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,28 +1,13 @@
+source("Loading the DataR")
 
+#plot.3
+plot(time_set, set$Sub_metering_1, type = "l",xlab = "", ylab = "Energy sub metering" )
+lines(time_set, set$Sub_metering_2, type = "l", col = "red")
+lines(time_set, set$Sub_metering_3, type = "l", col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty = 1, lwd = 1.5, cex = 0.7, col =c("black", "red", "blue"),
+       y.intersp = 0.5)
 
-#set working directory to file location
-setwd("H:/datasciencecoursera/Exploratory Data/exdata-data-household_power_consumption")
-#read in file
-householdFile <- "household_power_consumption.txt"
-plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
-## set time variable
-FebData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
-SetTime <-strptime(paste(FebData$Date, FebData$Time),"%d/%m/%Y %H:%M:%S")
-
-## Converting dates
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
-
-## Converting dates
-datetime <- paste(as.Date(data$Date), data$Time)
-FebData$Datetime <- as.POSIXct(datetime)
-
-## Plot 3
-with(FebData, {
-  plot(Sub_metering_1~Datetime, type="l",
-       ylab="Global Active Power (kilowatts)", xlab="")
-  lines(Sub_metering_2~Datetime,col='Red')
-  lines(Sub_metering_3~Datetime,col='Blue')
-})
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
-       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+#Saving file
+dev.copy(png,  file = "plot3.png", height = 480, width = 480)
+dev.off()     
