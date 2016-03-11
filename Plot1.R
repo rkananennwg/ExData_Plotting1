ef371015f1b8d54b691f41438f6f3497c9406d96
rkananennwg <rkananen@newgistics.com>
2016-03-10 21:22:21 -0600

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,16 +1,13 @@
 
 
-#set working directory to file location
-setwd("H:/datasciencecoursera/Exploratory Data/exdata-data-household_power_consumption")
-#read in file
-householdFile <- "household_power_consumption.txt"
-plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
-## set time variable
-FebData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
-SetTime <-strptime(paste(FebData$Date, finalData$Time),"%d/%m/%Y %H:%M:%S")
+source("Loading the Data.R") 
+#plot.1
+hist(set$Global_active_power, main = "Global Active Power",
+     xlab = "Global Active Power(kilowatts)", col = "red")
 
-png(filename='plots/plot1.png',width=480,height=480,units='px')
-
+#Saving the file
+dev.copy(png,  file = "plot1.png", height = 480, width = 480)
+dev.off()
 ## Generating Plot 1
 hist(FebData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 
