8b137c37e05226778b0534e50af889f6ea776a9d
rkananennwg <rkananen@newgistics.com>
2016-03-20 19:37:57 -0500

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,13 +1,10 @@
-source("Loading the DataR")
-
-#plot.3
-plot(time_set, set$Sub_metering_1, type = "l",xlab = "", ylab = "Energy sub metering" )
-lines(time_set, set$Sub_metering_2, type = "l", col = "red")
-lines(time_set, set$Sub_metering_3, type = "l", col = "blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       lty = 1, lwd = 1.5, cex = 0.7, col =c("black", "red", "blue"),
-       y.intersp = 0.5)
-
-#Saving file
-dev.copy(png,  file = "plot3.png", height = 480, width = 480)
-dev.off()     
+plot3 <- function() {
+  plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+  lines(df$timestamp,df$Sub_metering_2,col="red")
+  lines(df$timestamp,df$Sub_metering_3,col="blue")
+  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
+  dev.copy(png, file="plot3.png", width=480, height=480)
+  dev.off()
+  cat("plot3.png has been saved in", getwd())
+}
+plot3()
