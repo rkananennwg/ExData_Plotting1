6cdc2009dabcb969aac227b511445d431ff9c8e7
rkananennwg <rkananen@newgistics.com>
2016-03-20 19:28:52 -0500

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,15 +1,12 @@
 
 
-source("Loading the Data.R") 
-#plot.1
-hist(set$Global_active_power, main = "Global Active Power",
-     xlab = "Global Active Power(kilowatts)", col = "red")
-
-#Saving the file
-dev.copy(png,  file = "plot1.png", height = 480, width = 480)
-dev.off()
-## Generating Plot 1
-hist(FebData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+plot1 <- function() {
+  hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
+  dev.copy(png, file="plot1.png", width=480, height=480)
+  dev.off()
+  cat("Plot1.png has been saved in", getwd())
+}
+plot1()
 
 
 
