064b3977f376eaf74ff21749a3dc52be1d006a62
rkananennwg <rkananen@newgistics.com>
2016-03-20 19:37:02 -0500

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,17 +1,23 @@
-source("Loading the Data.R")
-
-#plot 4
-par(mfrow = c(2, 2)) 
-plot(time_set, set$Global_active_power, type="l", xlab="", ylab="Global Active Power", cex=0.2)
-plot(time_set, set$Voltage, type="l", xlab="datetime", ylab="Voltage")
-plot(time_set, set$Sub_metering_1, type = "l",xlab = "", ylab = "Energy sub metering" )
-lines(time_set, set$Sub_metering_2, type = "l", col = "red")
-lines(time_set, set$Sub_metering_3, type = "l", col = "blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       lty = 1, lwd = 1.5, cex = 0.7, col =c("black", "red", "blue"), bty = "n",
-       y.intersp = 0.2)
-plot(time_set, set$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
-
-#Saving File
-dev.copy(png,  file = "plot4.png", height = 480, width = 480)
-dev.off() 
+plot4 <- function() {
+        par(mfrow=c(2,2))
+        
+        ##PLOT 1
+        plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+        ##PLOT 2
+        plot(df$timestamp,df$Voltage, type="l", xlab="datetime", ylab="Voltage")
+        
+        ##PLOT 3
+        plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+        lines(df$timestamp,df$Sub_metering_2,col="red")
+        lines(df$timestamp,df$Sub_metering_3,col="blue")
+        legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), bty="n", cex=.5) #bty removes the box, cex shrinks the text, spacing added after labels so it renders correctly
+        
+        #PLOT 4
+        plot(df$timestamp,df$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+        
+        #OUTPUT
+        dev.copy(png, file="plot4.png", width=480, height=480)
+        dev.off()
+        cat("plot4.png has been saved in", getwd())
+}
+plot4()
